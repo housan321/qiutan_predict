4aba5f2b486fcdb718bf9a154ca49b90b5805e96
housan321 <housan321@163.com>
2019-08-15 15:27:00 +0800

    数据库的表按时间排序

--- begin.py
+++ begin.py
@@ -39,7 +39,7 @@ def union_save_database(db):
                 'score.HTGS, score.ATGS, score.HTGC, score.ATGC, score.HTGD, score.ATGD, score.HTP, score.ATP, score.HomeLP, score.AwayLP, score.VTFormPtsStr, score.HTFormPtsStr, score.ATFormPtsStr, ' \
                 'oz.oz_home0_mean, oz.oz_draw0_mean, oz.oz_away0_mean, oz.oz_home9_mean, oz.oz_draw9_mean, oz.oz_away9_mean, oz.oz_home0_std, oz.oz_draw0_std, oz.oz_away0_std, oz.oz_home9_std, oz.oz_draw9_std, oz.oz_away9_std,' \
                 ' az.az_home0_mean, az.az_size0_mean, az.az_away0_mean, az.az_home9_mean, az.az_size9_mean, az.az_away9_mean, az.az_home0_std, az.az_size0_std, az.az_away0_std, az.az_home9_std, az.az_size9_std, az.az_away9_std, az.az_value0, az.az_value9 ' \
-                'FROM all_match_score score JOIN  all_match_oz_odds oz  ON score.bs_num_id=oz.bs_num_id JOIN all_match_az_odds az ON score.bs_num_id=az.bs_num_id ;'
+                'FROM all_match_score score JOIN  all_match_oz_odds oz  ON score.bs_num_id=oz.bs_num_id JOIN all_match_az_odds az ON score.bs_num_id=az.bs_num_id order by bs_time;'
     db.union_item(union_sql)
 
     if (os.path.exists('D:/qiutan_predict/prediction/datasets/taday_matchs.csv')):
@@ -58,12 +58,12 @@ def union_save_database(db):
 
 def predict_match(league_model):
     loc = r"D:\qiutan_predict\prediction\\"
-    loadname = loc + r"datasets\taday_matchs.csv"
-    taday_matchs = pd.read_csv(loadname, encoding="gbk")
+    loadfile = loc + r"datasets\taday_matchs.csv"
+    taday_matchs = pd.read_csv(loadfile, encoding="gbk")
     match_info = taday_matchs[['league', 'hometeam', 'awayteam', 'bs_time']]
 
     predict = prediction()
-    X_all = predict.extract_feature(filename)
+    X_all = predict.extract_feature(loadfile)
 
     # load xgboost model
     xgboost_model = joblib.load('./prediction/model/xgboost_joblib({}).dat'.format(league_model))
@@ -71,7 +71,7 @@ def predict_match(league_model):
     y_pred = xgboost_model.predict(X_all)
     res = pd.DataFrame(y_pred, columns=['y_pred'])
     pred_res =pd.concat([match_info, res], axis=1)
-    nowtime = time.strftime('%Y%m%dH', time.localtime())
+    nowtime = time.strftime('%Y%m%d%H', time.localtime())
     pred_res.to_csv("./prediction/datasets/prediction{}.csv".format(nowtime), encoding = "gbk", index=None)
 
 
@@ -80,16 +80,16 @@ def predict_match(league_model):
 if __name__=='__main__':#在win系统下必须要满足这个if条件
     db = MySql('localhost', 'root', '123456', 'qiutan', 3306)
 
-    #1 先清除旧数据
-    delete_database(db)
-
-    #2 开启爬虫进程
-    print('%',os.getpid())#主进程号
-    spi = Process(target=spider_get_data)#创建子进程对象，并传参args=(X,),注意只传一个参数的时候必须在参数后加逗号
-    spi.start()
-    spi.join()
-    print('爬虫进程结束...')
-    print('%', os.getppid())#主进程的父进程
+    # #1 先清除旧数据
+    # delete_database(db)
+    #
+    # #2 开启爬虫进程
+    # print('%',os.getpid())#主进程号
+    # spi = Process(target=spider_get_data)#创建子进程对象，并传参args=(X,),注意只传一个参数的时候必须在参数后加逗号
+    # spi.start()
+    # spi.join()
+    # print('爬虫进程结束...')
+    # print('%', os.getppid())#主进程的父进程
 
     #3 合并数据，保存到文件
     union_save_database(db)
